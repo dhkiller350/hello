60ba34a5eaf34583247bd39aaf1dd8c53050c11f
dhkiller350 <100392451+dhkiller350@users.noreply.github.com>
2024-05-12 14:19:16 -0500

    Update script.js'

--- script.js
+++ script.js
@@ -211,3 +211,27 @@ function _(e) {
 navigator.getBattery ? navigator.getBattery().then(_) : m && _(m)
 
 console.log("INFORMATION: ",f)
+
+
+const getUA = () => {
+    let device = "Unknown";
+    const ua = {
+        "Generic Linux": /Linux/i,
+        "Android": /Android/i,
+        "BlackBerry": /BlackBerry/i,
+        "Bluebird": /EF500/i,
+        "Chrome OS": /CrOS/i,
+        "Datalogic": /DL-AXIS/i,
+        "Honeywell": /CT50/i,
+        "iPad": /iPad/i,
+        "iPhone": /iPhone/i,
+        "iPod": /iPod/i,
+        "macOS": /Macintosh/i,
+        "Windows": /IEMobile|Windows/i,
+        "Zebra": /TC70|TC55/i,
+    }
+    Object.keys(ua).map(v => navigator.userAgent.match(ua[v]) && (device = v));
+    return device;
+}
+
+console.log(getUA());
